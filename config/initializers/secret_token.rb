2a66c95113fd5445c5e43acb33c96a23142b1f18
Chris Allnutt <chris@chrisallnutt.com>
2013-02-07 15:03:28 -0500

    rails 4 initial structure commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Trackfit::Application.config.secret_key_base = '152207bb0e508a0da5fc48e40c1e70aa7e96cecaba4d11a7310aaf0690b82fe80c40d099d187aba4d6ac9efb3b1442ea0bc79126975b6537e2f3b1081f06779b'
