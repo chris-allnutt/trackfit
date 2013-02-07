5efb8a1f6bc96db8709c1a18110ce91c3c8f596d
Chris Allnutt <chris@chrisallnutt.com>
2013-02-07 17:21:38 -0500

    Added email validator Added model tests for user

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,8 @@
 class User < ActiveRecord::Base
+  validates :email_address,
+    :presence => true,
+    :uniqueness => true,
+    :email => true
+
   has_many :workouts
 end
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,21 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "does not save when given an invalid user email address " do
+    user = User.new(:email_address => "not a valid email")
+    assert !user.save, "Allowed an invalid email address to be saved"
+  end
+
+  test "saves record with a valid email address" do
+    user = User.new(:email_address => "a@validemail.com")
+    assert user.save, "Did not allow a properly formatted e-mail address to be saved"
+  end
+
+  test "does not save record with a duplicate email address" do
+    user = User.create(:email_address => "a@validemail.com")
+    user = User.new(:email_address => "a@validemail.com")
+    assert !user.save, "Saved duplicate e-mail address"
+  end
+
+
 end
