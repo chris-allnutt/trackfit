ef58b907d88f84fa37ee36ba432a0581c0206f2c
Chris Allnutt <chris@clownfishdesigns.com>
2013-02-14 00:46:57 -0500

    precompiles assets in production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -17,7 +17,7 @@ Trackfit::Application.configure do
   # Enable Rack::Cache to put a simple HTTP cache in front of your application
   # Add `rack-cache` to your Gemfile before enabling this.
   # For large-scale production use, consider using a caching reverse proxy like nginx, varnish or squid.
-  # config.action_dispatch.rack_cache = true
+  config.action_dispatch.rack_cache = true
 
   # Disable Rails's static asset server (Apache or nginx will already do this).
   config.serve_static_assets = false
@@ -59,7 +59,7 @@ Trackfit::Application.configure do
 
   # Precompile additional assets.
   # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
-  # config.assets.precompile += %w( search.js )
+  config.assets.precompile += %w( application.css )
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
