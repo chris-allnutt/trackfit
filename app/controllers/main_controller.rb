62fc7f0870ed439b8886228565cee7907b591aa4
Chris Allnutt <chris@chrisallnutt.com>
2013-02-18 14:46:06 -0500

    Use relative dates on the main screen

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -4,15 +4,15 @@ class MainController < ApplicationController
     @workout_days = [
       {
         :name => 'Today',
-        :date => Date.today.to_s
+        :date => 0
       },
       {
         :name => 'Yesterday',
-        :date => Date.today-1
+        :date => '-1'
       },
       {
         :name => 'Two Days Ago',
-        :date => Date.today-2
+        :date => '-2'
       }
     ]
   end
