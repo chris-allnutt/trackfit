345bce2eb8c2eeb71186f76bb8c27df9b055bcc6
Chris Allnutt <chris@chrisallnutt.com>
2013-03-13 16:38:35 -0400

    Added custom date range option to standings page

--- app/controllers/report_controller.rb
+++ app/controllers/report_controller.rb
@@ -1,16 +1,34 @@
 class ReportController < ApplicationController
   def standings
+    @start_date = nil
+    @end_date = nil
     beginning_of_month = Date.current().at_beginning_of_month
 
     @report_data = []
 
+    if !params[:start_date].blank? && !params[:end_date].blank?
+      @start_date = params[:start_date]
+      @end_date = params[:end_date]
+    end
+
     User.all().each do |user|
       user_name = user.email_address.split('@').first
 
-      total_workouts = Workout.where(
-        "user_id = :user_id AND worked_out_on >= :worked_out_on",
-        {:user_id => user.id, :worked_out_on => beginning_of_month.to_s}
-      ).count
+      if !@start_date.nil? && !@end_date.nil?
+        total_workouts = Workout.where(
+          "user_id = :user_id AND (worked_out_on >= :start_date AND worked_out_on <= :end_date)",
+          {
+            :user_id => user.id, 
+            :start_date => @start_date,
+            :end_date => @end_date
+          }
+        ).count  
+      else
+        total_workouts = Workout.where(
+          "user_id = :user_id AND worked_out_on >= :worked_out_on",
+          {:user_id => user.id, :worked_out_on => beginning_of_month.to_s}
+        ).count
+      end
 
       @report_data.push({
         "name" => user_name,
